4fabb3d794bf11c74cc05cb103bc54247e746182
Cédric Marcelly <36482580+cmarcelly-plu@users.noreply.github.com>
2018-05-24 14:57:50 -0400

    menu styled

--- src/components/AlbumsMenu.vue
+++ src/components/AlbumsMenu.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="albums-menu">
-    <p>{{ msg }}</p>
     <ul>
       <li>
-        <a href="#"><img src="./assets/img/madonna.jpg" alt=""></a>
+        <a href="#"><img src="https://d24jnm9llkb1ub.cloudfront.net/icpn/00028947981039/00028947981039-cover-225.jpg" alt=""></a>
       </li>
       <li>
-        <a href="#"><img src="./assets/img/madonna.jpg" alt=""></a>
+        <a href="#"><img src="https://d24jnm9llkb1ub.cloudfront.net/icpn/00028948340941/00028948340941-cover-225.jpg" alt=""></a>
       </li>
       <li>
-        <a href="#"><img src="./assets/img/madonna.jpg" alt=""></a>
+        <a href="#"><img src="https://s3.amazonaws.com/preview.prostudiomasters.com/icpn/00602567502432/00602567502432-cover-225.jpg" alt=""></a>
       </li>
       <li>
-        <a href="#"><img src="./assets/img/madonna.jpg" alt=""></a>
+        <a href="#"><img src="https://s3.amazonaws.com/preview.prostudiomasters.com/icpn/00602567723905/00602567723905-cover-225.jpg" alt=""></a>
       </li>
     </ul>
   </div>
@@ -35,6 +34,37 @@ export default {
   position: absolute;
   bottom: 20px;
   width: 100%;
-  border: solid 1px red;
+  background: rgba(0, 0, 0, 0.2);
+}
+
+ul {
+  font-size: 0;
+  display: block;
+  list-style-type: none;
+  margin: 10px auto;
+  padding: 0;
+  text-align: center;
+  width: 50%;
+}
+
+ul li {
+  display: inline-block;
+  padding: 0 10px 0;
+  width: 25%;
+  // width: 80%;
+}
+
+ul li a {
+  display: block;
+}
+
+ul li a:hover {
+  border: solid 2px white;
+}
+
+ul li img {
+  display: block;
+  width: 100%;
+  height: auto;
 }
 </style>
